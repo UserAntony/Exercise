f7a18508ef6ae799bb7438baa78c269fdec43010
Anthony Pereira Souza <126885345+UserAntony@users.noreply.github.com>
2023-03-28 14:52:35 -0300

    Add files via upload

---
+++ idades.java
@@ -0,0 +1,41 @@
+	package idades;
+	
+	import java.util.Locale;
+	import java.util.Scanner;
+	
+	public class idades {
+	
+		public static void main(String[] args) {
+			
+			Locale.setDefault(Locale.US);
+			Scanner sc = new Scanner(System.in);
+				
+			String nome1, nome2;
+				int  idade1, idade2;
+				double media ;
+				
+				System.out.println("Dados da primeira pessoa:");
+				System.out.print("Nome: ");
+				nome1 = sc.nextLine();
+				System.out.println("Idade: ");
+				idade1= sc.nextInt();
+				
+	
+				System.out.println("Dados da segunda pessoa:");
+				System.out.print("Nome: ");
+				sc.nextLine(); 
+				nome2 = sc.nextLine();
+				System.out.println("Idade: ");
+				idade2= sc.nextInt();
+				
+				media =  (19 + 20) / 2.0;
+				
+	
+				System.out.println("A idade media " + nome1+ "e"+ nome2 + " eh de " +  String.format("%.1f", media)+" anos");
+			
+	
+	
+		}
+	
+	
+	}
